86f64183d11da4fef7f775071d6dae134eef71d6
Rosalie Newman <rosalienewman0@gmail.com>
2023-03-21 12:46:46 -0400

    done but glitchy

--- app.js
+++ app.js
@@ -1,21 +1,25 @@
 // Create our observer
-const observer = new IntersectionObserver((entries) => {
-  entries.forEach((entry) => {
-    console.log(entry);
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      //console.log(entry);
 
-    // if it's intersecting AKA its visible on the screen...
-    if (entry.isIntersecting) {
-      entry.target.classList.add("show"); // add a class called 'show' to it
-    }
-    // if you want the animation to replay, need to also remove show
-    else {
-      entry.target.classList.remove("show");
-    }
-  });
-});
+      // if it's intersecting AKA its visible on the screen...
+      if (entry.isIntersecting) {
+        console.log("is hit");
+        entry.target.classList.add("show"); // add a class called 'show' to it
+      }
+      // if you want the animation to replay, need to also remove show
+      else {
+        entry.target.classList.remove("show");
+      }
+    });
+  }
+  // { threshold: 0.01 }
+);
 
 // Grab our elements we want to apply the observer to
 const hiddenElements = document.querySelectorAll(".hidden");
-
+console.log(hiddenElements);
 // for each element in hidden elements, apply the observer
-hiddenElements.forEach((element) => observer.observe(element));
+hiddenElements.forEach((el) => observer.observe(el));
